088b2de369b30c8c2587cf2b0d1c105249a59797
Pavel Orestov <VorteX456@Rambler.ru>
2018-10-06 21:41:32 +0300

    Homework 1 done

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿//а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле
+//r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). 
+//Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+//б) * Выполните предыдущее задание, оформив вычисления расстояния между точками в виде метода;
+
+//Павел Орестов
+
+using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату X1:");
+            float x1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите координату Y1:");
+            float y1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите координату X2:");
+            float x2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите координату Y2:");
+            float y2 = Convert.ToSingle(Console.ReadLine());
+
+            //а
+            Console.WriteLine("Расстояние между точками: {0:F2}", Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            //б
+            Console.WriteLine("Расстояние между точками: {0:F2}", CalculateRange(x1, y1, x2, y2));
+
+            Console.ReadKey();
+        }
+
+        static double CalculateRange(float x1, float y1, float x2, float y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿//Написать программу обмена значениями двух переменных.
+//а) с использованием третьей переменной;
+//б) * без использования третьей переменной.
+
+//Павел Орестов
+
+using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Введите число a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //а
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Меняем местами...");
+            Console.WriteLine($"a: {a}\tb: {b}");
+
+            Console.WriteLine("Возвращаем как было...");
+            c = a; a = b; b = c;
+            Console.WriteLine($"a: {a}\tb: {b}");
+
+            //б
+            Console.WriteLine("Меняем местами вторым способом...");
+            b = a ^ b;
+            a = a ^ b;
+            b = a ^ b;
+            Console.WriteLine($"a: {a}\tb: {b}");
+
+            Console.ReadKey();
+        }
+    }
+}
